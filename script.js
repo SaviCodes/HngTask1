72cd680942e3e2e51c4b7907006446e086e6adc6
saviour <saviourtari@gmail.com>
2023-09-10 18:29:47 +0100

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 
 const d = new Date();
 let day = weekday[d.getDay()];
-document.getElementById("demo").innerHTML = day;
+document.getElementById("time").innerHTML = day;
 
 
 function addZero(x,n) {
@@ -21,7 +21,7 @@ let s = addZero(t.getSeconds(),2);
 let ms = addZero(t.getMilliseconds(),3);
 
 let time = h + ":" + m + ":" + s + ":" + ms;
-document.getElementById("dd").innerHTML = time;
+document.getElementById("date").innerHTML = time;
 
 setTimeout (updateClock,1);
 }
