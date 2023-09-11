55329ce6c74fcee68f61df4e139d23486913aaad
saviour <saviourtari@gmail.com>
2023-09-11 07:59:21 +0100

    update

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",]
+const dateClock = setInterval(function dateTime() {
+    const today = new Date();
+    let date = today.getDate();
+    let day = weekdays[today.getDay()];
+    let month = monthNames[today.getMonth()];
+  
 
+  }, 1000);
+  
+          setInterval(()=>{
+              const time = document.querySelector(".time");
+          let date = new Date();
+          let hours = date.getHours()
+          let minutes = date.getMinutes()
+          let seconds = date.getSeconds()
+          let milliseconds = date.getMilliseconds();
+          let day_night = 'AM';
+          if (hours > 12){
+              day_night = 'PM'
+              hours = hours - 12;
+          }
+          if (minutes < 10){
+              minutes = '0' + minutes;
+          }
+          if (hours < 10){
+              hours = '0' + hours;
+          }
+          if (seconds < 10){
+              seconds = '0' + seconds;
+          }
+          if (milliseconds < 10){
+              seconds = '0' + milliseconds;
+          }
+         
+          time.textContent = 'Current UTC Time:' + hours + ':' + minutes + ':' + seconds +':'+ milliseconds + ' ' + day_night;
+          })
+  
+  
+const dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
 
-const d = new Date();
-let day = weekday[d.getDay()];
-document.getElementById("time").innerHTML = day;
-
-
-function addZero(x,n) {
-    while (x.toString().length < n) {
-        x = "0" + x;
-    }
-    return x;
-}
-
-function updateClock() {
-const t = new Date();
-let h = addZero(t.getHours(),2);
-let m = addZero(t.getMinutes(),2);
-let s = addZero(t.getSeconds(),2);
-let ms = addZero(t.getMilliseconds(),3);
-
-let time = h + ":" + m + ":" + s + ":" + ms;
-document.getElementById("date").innerHTML = time;
-
-setTimeout (updateClock,1);
-}
-
-updateClock();
+ const today = new Date();
+ const currentDay = dayOfWeek[today.getUTCDay()];
+ document.querySelector('[data-testid="currentDayOfTheWeek"').textContent = currentDay;
